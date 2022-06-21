bcf0edfbadb69be28a6154708c82d0e2e53c7815
Darien Tsai <smartdragondt@gmail.com>
2022-06-21 16:08:40 -0700

    add start script to npm script

--- index.ts
+++ index.ts
@@ -1,24 +1,21 @@
-import DiscordJS, { Intents } from 'discord.js'
-import dotenv from 'dotenv'
-dotenv.config()
+import DiscordJS, { Intents } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
-})
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-client.on('ready', () => {
-    console.log('The bot is ready')
-})
+client.on("ready", () => {
+  console.log("The bot is ready");
+});
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong',
-        })
-    }
-})
+client.on("messageCreate", (message) => {
+  if (message.content === "ping") {
+    message.reply({
+      content: "pong",
+    });
+  }
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
